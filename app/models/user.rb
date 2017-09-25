085cec59601f6eddad952c4e99e699bb34365232
Kawahara Airi <airi.kawahara@pa-rk.co.jp>
2017-09-25 15:36:47 +0900

    Railsチュートリアル6.2.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ApplicationRecord
-  validates(:name,  presence: true, length: { maximum: 50 })
+  # before_saveというコールバックを使い、オブジェクトが保存される際にemail属性を小文字に変換
+  # Userモデルでは右辺のselfを省略することができる(左辺は省略することができない)
+  # before_save { self.email = email.downcase }
+  # 末尾に「!」を付け加えることによりemail属性を直接変更できるようになる
+  before_save { email.downcase! }
+  validates(
+    :name,
+    presence: true,
+    length: { maximum: 50 }
+  )
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates(:email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX })
+  validates(
+    :email,
+    presence: true,
+    length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    # メールアドレスの大文字小文字を無視した一意性の検証
+    uniqueness: { case_sensitive: false }
+  )
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -44,4 +44,20 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
+
+  test "email addresses should be unique" do
+    # dupは同じ属性を持つデータを複製するためのメソッド
+    duplicate_user = @user.dup
+    # 通常メールアドレスでは大文字小文字が区別されないため大文字を区別せずにテストを行う
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
 end

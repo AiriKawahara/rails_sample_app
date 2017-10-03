0f43231829d66d5d1c40044f75289058d13f011a
kawahara.airi <airi.kawahara@pa-rk.co.jp>
2017-10-03 12:43:40 +0900

    現状反映

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -57,10 +57,7 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-<<<<<<< HEAD
-=======
   # email属性の一意性のテスト
->>>>>>> 6modeling_users
   test "email addresses should be unique" do
     # dupは同じ属性を持つデータを複製するためのメソッド
     duplicate_user = @user.dup
@@ -70,18 +67,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-<<<<<<< HEAD
-=======
   # email属性の小文字化に対するテスト
->>>>>>> 6modeling_users
   test "email addresses should be saved as lower-case" do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
-<<<<<<< HEAD
-=======
 
   # パスワードの存在性のテスト
   test "password should be present (nonblank)" do
@@ -94,5 +86,4 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
->>>>>>> 6modeling_users
 end

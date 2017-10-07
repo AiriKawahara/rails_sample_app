ac23dff43aa8eafecd81acf32e96e9b7dc828d2a
Airi Kawahara <kwhrair928@gmail.com>
2017-10-07 10:42:26 +0900

    Railsチュートリアル7.3.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # 実装は終わっていないことに注意
+    @user = User.new(user_params)    # 実装は終わっていないことに注意
     if @user.save
-      # 保存の成功をここで扱う。
+      # 保存の成功をここで扱う
     else
       # 保存に失敗した場合は登録ページに遷移する
       render 'new'
     end
   end
+
+  private
+
+    def user_params
+      params
+      .require(:user)
+      .permit(:name, :email, :password, :password_confirmation)
+    end
 end

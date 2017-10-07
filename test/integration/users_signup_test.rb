aadc9fd6a0d4c347212e2165501fc794e98d4ef7
Airi Kawahara <kwhrair928@gmail.com>
2017-10-08 00:13:27 +0900

    Railsチュートリアル7.4.4

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
 
+    # 登録失敗時のテスト
     # テスト実行前後でUser.countが変わらないことをテストする
     assert_no_difference 'User.count' do
       post signup_path,
@@ -17,7 +18,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       }
     end
 
-    # 送信失敗の場合はnewアクションが再描画されていることをテストする
+    # 登録失敗の場合はnewアクションが再描画されていることをテストする
     assert_template('users/new')
 
     # エラーメッセージをテストする
@@ -26,5 +27,29 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 
     # POSTが送信されているURLが正しいかをテストする
     assert_select('form[action="/signup"]')
+  end
+
+  test "valid signup information" do
+    get signup_path
+
+    # 登録成功時のテスト
+    # テスト実行前後でUser.countが1異なることをテストする
+    assert_difference 'User.count', 1 do
+      post signup_path,
+      params: {
+        user: {
+          name:                  "Example User",
+          email:                 "user@example.com",
+          password:              "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+    # POSTリクエストを送信した結果を見て指定したリダイレクト先に移動するメソッド
+    follow_redirect!
+    assert_template('users/show')
+
+    # flashのテスト
+    assert_not flash.empty?
   end
 end

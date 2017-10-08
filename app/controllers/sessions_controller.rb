4a889831a1074c93242e90e3193d17ad7fe0042b
Airi Kawahara <kwhrair928@gmail.com>
2017-10-09 02:55:43 +0900

    Railsチュートリアル8.1.4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
-      # エラーメッセージを作成する
+      # エラーメッセージを作成する(現在は正しくないコード)
+      # リダイレクトを使った時とは異なりrenderメソッドではリクエストのメッセージが消えない
+      flash[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end

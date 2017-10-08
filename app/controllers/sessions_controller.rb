68cd9ef05d368902ad28204065277cfab13031b0
Airi Kawahara <kwhrair928@gmail.com>
2017-10-09 03:43:49 +0900

    Railsチュートリアル8.2.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     # authenticateメソッドは認証に失敗した時にfalseを返す
     if user && user.authenticate(params[:session][:password])
+      # SessionsHelperのloginメソッドを呼び出しセッションを登録する
+      log_in(user)
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      redirect_to user
     else
       # flash.nowはレンダリングが終わっているページで特別にフラッシュメッセージを表示することができる
       # その後リクエストが発生した時に消滅する
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,8 @@
 module SessionsHelper
+  # 渡されたユーザーでログインする
+  def log_in(user)
+    # ユーザーのブラウザ内の一時cookiesに暗号化済みのユーザーIDが自動で作成される
+    # この後のページで、session[:user_id]を使ってユーザーIDを元通りに取り出すことができる
+    session[:user_id] = user.id
+  end
 end

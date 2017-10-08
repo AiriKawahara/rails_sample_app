7420d185a13e4f8838393affb26a0246e1e57d28
Airi Kawahara <kwhrair928@gmail.com>
2017-10-09 02:42:23 +0900

    Railsチュートリアル8.1.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    # authenticateメソッドは認証に失敗した時にfalseを返す
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      # エラーメッセージを作成する
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end

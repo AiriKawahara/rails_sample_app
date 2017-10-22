59e8bab70d4ffac451344189d3b175bbff806261
Airi Kawahara <kwhrair928@gmail.com>
2017-10-22 17:25:26 +0900

    Railsチュートリアル10.4.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   # indexアクションとeditアクションとupdateアクションを実行する直前にlogged_in_userアクションを実行させる
   before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:delete]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -48,6 +49,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
 
     def user_params
@@ -80,5 +87,10 @@ class UsersController < ApplicationController
       # ログイン中のユーザーと違うユーザーの編集ページに遷移しようとした場合
       # root_urlにリダイレクトする
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end

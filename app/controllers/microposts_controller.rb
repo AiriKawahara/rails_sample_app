3901b49ecfff7d87f27931b817be36cf934990d6
Airi Kawahara <kwhrair928@gmail.com>
2017-11-23 20:01:44 +0900

    Railsチュートリアル13.3.4

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     # 新しいマイクロポストをbuildするためにUser/Mcropost関連付けを使っている
@@ -15,6 +16,13 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    # request.referrerメソッドは1つ前のURLを返す
+    # DELETEリクエストが発行されたページに戻すことができる
+    redirect_to request.referrer || root_url
+    # 下のコードでもうまくいく
+    # redirect_back(fallback_location: root_url)
   end
 
   private
@@ -23,5 +31,13 @@ class MicropostsController < ApplicationController
       # Strong Parameters(DBへ追加したり更新するパラメータを制限する仕組み)
       # を使ってマイクロポストのcontent属性だけがWeb経由で変更可能にしている
       params.require(:micropost).permit(:content)
+    end
+
+    def correct_user
+      # 関連付けを使ってマイクロポストを見つけることによって
+      # 他のユーザーのマイクロポストを削除しようとすると自動的に失敗するようになる
+      # 他のユーザーのマイクロポストを削除しようとした場合は@micropostがnilになる
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end

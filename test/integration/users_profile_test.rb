371ca491637f583f9a399d45d31910ad6220fabb
Airi Kawahara <kwhrair928@gmail.com>
2017-12-03 02:09:09 +0900

    Railsチュートリアル14.2.2

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -28,5 +28,15 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    # プロフィールページにフォロー数フォロワー数が表示されているかテストする
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end
+
+  test "count relationships in home page" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
   end
 end

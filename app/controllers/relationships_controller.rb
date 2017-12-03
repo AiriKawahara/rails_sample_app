4dd9958bfce3a70af6ab4ceed67b0f8ac52aedb9
Airi Kawahara <kwhrair928@gmail.com>
2017-12-03 17:25:35 +0900

    Railsチュートリアル14.2.5

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,14 +2,25 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_to user
+    # Ajaxの場合はインスタンス変数を使う
+    # またビューで変数を使うためuserが@userに変わった
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+    # Ajaxリクエストに応答できるようにする
+    # リクエストの種類によって応答を場合分けする時はrespond_toメソッドを使う
+    # respond_toメソッドはブロック内のいずれかの1行が実行される
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_to user
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
